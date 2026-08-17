07f8eba70fd9345770f90f7e9635b0603c1e92de
Monyiii <paniaguathirdmail@gmail.com>
2026-08-16 20:39:29 -0600

    v0.03 Added a Fruit

--- Catchthefruit.py
+++ Catchthefruit.py
@@ -8,14 +8,19 @@ ventana = pygame.display.set_mode((800, 600))
 
 taza_x = 350
 taza_s = 420
+fruta_x = 400
+fruta_y = 100
+fruta_s = 300
 fps = 60
 
 def dibujar():
     ventana.fill((200, 200, 200))
     pygame.draw.rect(ventana, (100, 50, 200), (taza_x, 500, 100, 50))
+    pygame.draw.circle(ventana, (200, 0, 0), (fruta_x, fruta_y), 25)
 
 while True:
     dt = clock.tick(fps) / 1000
+
     if pygame.key.get_pressed()[pygame.K_LEFT] or pygame.key.get_pressed()[pygame.K_a]:
         taza_x -= taza_s * dt
     if pygame.key.get_pressed()[pygame.K_RIGHT] or pygame.key.get_pressed()[pygame.K_d]:
@@ -24,6 +29,12 @@ while True:
         taza_x = 800
     if taza_x > 800:
         taza_x = -100
+    
+    fruta_y += fruta_s * dt
+    if fruta_y >= 625:
+        fruta_y = -25
+        fruta_x = random.randint(25, 775)
+    
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             pygame.quit()
